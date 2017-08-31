b01d981b74465b4c5555bb4183dc02c457080f2c
shadowyy <yy225600@gmail.com>
2017-08-31 11:08:52 +0800

    config

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+/**
+ * ${DESCRIPTION}
+ * @author ${USER}
+ * @version ${DATE} ${TIME} 
+ */
+public class ${NAME} {
+    public void method(){
+        
+    }
+}
---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+/**
+ * ${DESCRIPTION}
+ * @author ${USER}
+ * @version ${DATE} ${TIME} 
+ */
+public interface ${NAME} {
+    String field;
+    
+    void method();
+}
